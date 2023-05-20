c24944860f331efc0e87b883795b6025b2c643ee
Arbaj Ali <arbajmessiali@outlook.com>
2023-05-20 12:20:17 +0530

    Setup The Environment

---
+++ backend/src/main/java/com/tclone/TcloneApplication.java
@@ -0,0 +1,13 @@
+package com.tclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tclone/TcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
